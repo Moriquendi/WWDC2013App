60b5fc26236cbc44cd6694ac34e3cb454748affa
Michal Smialko <smialko@futuresimple.com>
2013-04-28 11:44:19 +0200

    Updated projects description.

---
+++ WWDC2013App/WWDC2013App/Classes/Views/MSImageCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MSImageCollectionViewCell.h
+//  WWDC2013App
+//
+//  Created by Michał Śmiałko on 28.04.2013.
+//  Copyright (c) 2013 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSImageCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ WWDC2013App/WWDC2013App/Classes/Views/MSImageCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  MSImageCollectionViewCell.m
+//  WWDC2013App
+//
+//  Created by Michał Śmiałko on 28.04.2013.
+//  Copyright (c) 2013 MS. All rights reserved.
+//
+
+#import "MSImageCollectionViewCell.h"
+
+@implementation MSImageCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
